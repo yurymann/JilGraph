081943efd43be73bcc7f2aebeb339d9e83a9126a
yurymann <yurymann@gmail.com>
2013-12-22 01:25:46 +0000

    Quotes

--- js/jilParser.js
+++ js/jilParser.js
@@ -11,11 +11,18 @@ function removeComments(jilText) {
 }
 
 function quoteValues(jilText) {
-    var reg = new RegExp(/^\s*\w+:.*$/g);   
+    var resultText = "";
+    var reg = new RegExp(/^\s*\w+:.*$/gm);
+    
     var match;
+    //for (var match in matches) {
     while((match = reg.exec(jilText)) !== null) {
-        alert(match);
+        var line = match[0].replace(/\"/g, "\\\"").trim(); // Escape quotes
+        var propName = line.match(/\w+:/)[0];
+        var propValue = line.replace(/\w+:\s*/, "").trim();
+        
+        resultText = resultText + propName + ' "' + propValue + '",' + '\n';
     }
 
-    //return resultText;
+    return resultText.trim();
 }
