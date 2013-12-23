286d2586ef6d59a05c062ea951f743f022ba7ff1
yurymann <yurymann@gmail.com>
2013-12-23 23:13:07 +0000

    New stuff.

--- js/GraphBuilder.js
+++ js/GraphBuilder.js
@@ -1,8 +1,26 @@
 // parent: top-level DOM object for the graph
-function GraphBuilder(topContainer) {
+function GraphBuilder(jilArray, topContainer) {
+    this.topContainer = topContainer;
+    this.jilArray = jilArray;
     this.idPrefix = "div_";
 }
 
+GraphBuilder.prototype.draw = function() {
+    for (var job in getTopLevelJobs()) {
+        addJobWithChildren(job);
+    }
+}
+
+// Recursively adds a div for the job/box object.
+// Then, if the job is a box, adds divs for its children.
+GraphBuilder.prototype.addJobWithChildren = function(job) {
+    var div = addJobDiv(job, this.topContainer);
+    $.each(getBoxChildren(job), function() {
+        addJobWithChildren(child, parent);
+    });
+}
+
+// Creates div for the job or box and adds it to the parent container.
 GraphBuilder.prototype.addJobDiv = function(job, parent) {
     var div = $('<div>', 
     {   id: this.idPrefix + job.name, 
@@ -21,3 +39,20 @@ GraphBuilder.prototype.getJobClass = function(job, parent) {
     }
     throw "Unknown job type: " + jobType;
 }
+
+GraphBuilder.prototype.getTopLevelJobs = function() {
+    return $.grep(this.jilArray, function(job) {
+        return job.hasOwnProperty("box");
+    });
+}
+
+GraphBuilder.prototype.getBoxChildren = function(box) {
+    if (box.job_type != "b") {
+        throw "The provided object is not a box.";
+    }
+    
+    return $.grep(this.jilArray, function(job){
+        return job.box == box.name;
+    });
+}
+
